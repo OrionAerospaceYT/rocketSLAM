6bc1d163a7c931a8e1533b4576709dbb5b1eef00
mack.jonah@gmail.com <mack.jonah@gmail.com>
2021-04-26 00:29:49 -0700

    frame to previous frame tracking seems to be good-- looks like it is giving a good vector field of the motion of the cameras

--- features.py
+++ features.py
@@ -14,20 +14,23 @@ class Features:
         self.last_framer = 0
         self.first_frame = True
     def processFrame(self,stereo_frame):
-            self.frame_left = stereo_frame[0:1080,0:1920//2]
-            self.frame_right = stereo_frame[0:1080,1920//2:1920]
-            self.gray_left = cv.cvtColor(self.frame_left,cv.COLOR_BGR2GRAY)
-            self.gray_right = cv.cvtColor(self.frame_right,cv.COLOR_BGR2GRAY)
-            return (self.gray_left,self.last_framel),(self.gray_right,self.last_framer)
+        self.frame_left = stereo_frame[0:1080,0:1920//2]
+        self.frame_right = stereo_frame[0:1080,1920//2:1920]
+        #split the frame and convert to one channel
+        self.gray_left = cv.cvtColor(self.frame_left,cv.COLOR_BGR2GRAY)
+        self.gray_right = cv.cvtColor(self.frame_right,cv.COLOR_BGR2GRAY)
+        return (self.gray_left,self.last_framel),(self.gray_right,self.last_framer)
     def findPoints(self):
         kp_left,des_left = self.orb.detectAndCompute(self.gray_left,None)
         kp_right,des_right = self.orb.detectAndCompute(self.gray_right,None)
         self.left_feats = {"kp" : kp_left, "des" : des_left}
         self.right_feats = {"kp" : kp_right, "des" : des_right}
+        #extract and return the keypoints for both sides and also return previous kps
         return (self.left_feats,self.last_left),(self.right_feats,self.last_right)
     def matchFeats(self):
         matches_left =[]
         matches_right =[]
+        #only check for matches if we have a previous frame to compare to
         if not self.first_frame:
             matches_left = self.bf.match(self.left_feats["des"],self.last_left["des"])
             matches_right = self.bf.match(self.right_feats["des"],self.last_right["des"])
@@ -36,5 +39,8 @@ class Features:
         self.last_framer = self.gray_right
         self.last_framel = self.gray_left
         self.first_frame = False
+        #sort by shortest distances (using this we can then filter out weird matches)
+        matches_left = sorted(matches_left, key = lambda x:x.distance)
+        matches_right = sorted(matches_right, key = lambda x:x.distance)
         return matches_left,matches_right
   
--- main.py
+++ main.py
@@ -10,28 +10,60 @@ stereo_video = cv.VideoCapture('./Data/dual_camera_compressed.mp4')
 '''
 try adding a check for fiducials, if fioducials just track those and use this to calibrate
 if not fiducials run SLAM stuff 
-TO DO: 
-    A lot of clean up to do... But this serves as some proof that keypoints between previous frame and current frame track and match reasonably well-- This also isnt stereo slam rn-- its kind of just two slams-- need to look into whether this is a good approach or not
-'''
-extractor = Features()
-while(stereo_video.isOpened()):
-    ret,frame = stereo_video.read()
-    frames_left,frames_right = extractor.processFrame(frame)
-    left_feat,right_feat = extractor.findPoints()
-    left_m,right_m = extractor.matchFeats()
-    if left_feat[1] is not None:
-        gray_left = cv.drawMatches(frames_left[0],left_feat[0]["kp"],frames_left[1],left_feat[1]["kp"],left_m[:10],None,flags =cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        gray_right = cv.drawMatches(fr:ames_right[0],right_feat[0]["kp"],frames_right[1],right_feat[1]["kp"],right_m[:10],None,flags =cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        output = cv.vconcat([gray_left,cv.flip(gray_right,-1)])
-        output = cv.resize(output,(1280//2,1440//2))
-        cv.imshow('out', output)
-    if cv.waitKey(1) & 0xFF == ord('p'):
-        while not cv.waitKey(1) & 0xFF == ord('u'):
-            pass
-        
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
 
-left.release()
-right.release()
-cv.destroyAllWindows()
+TO DO:
+    -- this isnt really stereo-SLAM more like two SLAMs on both cameras rn (need to look into if this is an okay approach)
+    -- clean up main
+    -- pretty happy with features.py rn
+    -- add an array to store all previous "good" matches so that we can draw continuos lines and get better idea of the flow
+    -- look into filtering -- Essential Matrix and other Camera calibration 
+'''
+
+def main():
+    extractor = Features()
+    while(stereo_video.isOpened()):
+        ret,frame = stereo_video.read()
+        frames_left,frames_right = extractor.processFrame(frame)
+        fl = frames_left[0]
+        fr = frames_right[0]
+        left_feat,right_feat = extractor.findPoints()
+        left_m,right_m = extractor.matchFeats()
+        if left_feat[1] is not None:
+            for m in left_m[:20]:
+                p1 = m.trainIdx     
+                p2 = m.queryIdx
+                l1 = left_feat[0]["kp"][p2].pt
+                l1 = int(l1[0]),int(l1[1])
+                l2 = left_feat[1]["kp"][p1].pt
+                l2 = int(l2[0]),int(l2[1])
+                fl = cv.line(frames_left[0],l1,l2,(0,0,0),6)
+            for m in right_m[:20]:
+                p1 = m.trainIdx
+                p2 = m.queryIdx
+                l1 = right_feat[0]["kp"][p2].pt
+                l1 = int(l1[0]),int(l1[1])
+                l2 = right_feat[1]["kp"][p1].pt
+                l2 = int(l2[0]),int(l2[1])
+                fr = cv.line(fr,l1,l2,(0,0,0),6)
+            output = cv.vconcat([fl,cv.flip(fr,-1)])
+            output = cv.resize(output,(1280//2,1440//2))
+            cv.imshow('out', output)
+        if cv.waitKey(1) & 0xFF == ord('p'):
+            while not cv.waitKey(1) & 0xFF == ord('u'):
+                pass
+        
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    left.release()
+    right.release()
+    cv.destroyAllWindows()
+if __name__ == "__main__":
+    main()
+
+'''
+gray_left = cv.drawMatches(frames_left[0],left_feat[0]["kp"],frames_left[1],left_feat[1]["kp"],left_m[:10],None,flags = cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+gray_right = cv.drawMatches(frames_right[0],right_feat[0]["kp"],frames_right[1],right_feat[1]["kp"],right_m[:10],None,flags = cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+'''
+
+
