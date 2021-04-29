74af430979544b1cb9e2df80dd2846c0df41414b
mack.jonah@gmail.com <mack.jonah@gmail.com>
2021-04-28 23:13:07 -0700

    cleaned up repeated code

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ TO DO:
     -- this isnt really stereo-SLAM more like two SLAMs on both cameras rn (need to look into if this is an okay approach)
     -- clean up main
     -- pretty happy with features.py rn
-    -- add an array to store all previous "good" matches so that we can draw continuos lines and get better idea of the flow
+    -- look into VO and extracting data
     -- look into filtering -- Essential Matrix and other Camera calibration 
 '''
 
@@ -25,6 +25,22 @@ def distanceCheck(v,matches,threshold):
     if (((v[1][0] -v[0][0])**2)+((v[1][1]-v[0][1])**2)) < threshold*threshold: #for speed can change the thresh^2 to be computed once and then passed
         matches.append(v)
     return matches
+
+def vectorFlow(matches,feats, ret_vecs):
+    for m in matches[:20]:
+        p1 = m.trainIdx     
+        p2 = m.queryIdx
+        l1 = feats[0]["kp"][p2].pt
+        l1 = int(l1[0]),int(l1[1])
+        l2 = feats[1]["kp"][p1].pt
+        l2 = int(l2[0]),int(l2[1])
+        ret_vecs = distanceCheck((l1,l2),ret_vecs,match_thresh)
+    return ret_vecs
+
+def drawVectors(frame, ret_vecs):
+    for v in ret_vecs:    
+        frame = cv.line(frame,v[0],v[1],(255,255,255),6)
+    return frame
 
 def main():
     extractor = Features()
@@ -38,26 +54,10 @@ def main():
         left_feat,right_feat = extractor.findPoints()
         left_m,right_m = extractor.matchFeats()
         if left_feat[1] is not None:
-            for m in left_m[:20]:
-                p1 = m.trainIdx     
-                p2 = m.queryIdx
-                l1 = left_feat[0]["kp"][p2].pt
-                l1 = int(l1[0]),int(l1[1])
-                l2 = left_feat[1]["kp"][p1].pt
-                l2 = int(l2[0]),int(l2[1])
-                ret_vecs_l = distanceCheck((l1,l2),ret_vecs_l,match_thresh)
-            for v in ret_vecs_l:    
-                fl = cv.line(frames_left[0],v[0],v[1],(0,0,0),6)
-            for m in right_m[:20]:
-                p1 = m.trainIdx
-                p2 = m.queryIdx
-                l1 = right_feat[0]["kp"][p2].pt
-                l1 = int(l1[0]),int(l1[1])
-                l2 = right_feat[1]["kp"][p1].pt
-                l2 = int(l2[0]),int(l2[1])
-                ret_vecs_r = distanceCheck((l1,l2),ret_vecs_r,match_thresh)
-            for v in ret_vecs_r:
-                fr = cv.line(fr,v[0],v[1],(0,0,0),6)
+            ret_vecs_r = vectorFlow(right_m,right_feat,ret_vecs_r)
+            ret_vecs_l = vectorFlow(left_m, left_feat,ret_vecs_l)
+            fl = drawVectors(fl,ret_vecs_l)
+            fr = drawVectors(fr,ret_vecs_r)
             output = cv.vconcat([fl,cv.flip(fr,-1)])
             output = cv.resize(output,(1280//2,1440//2))
             cv.imshow('out', output)
